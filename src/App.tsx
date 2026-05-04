6df6ff6e64c219dba8c5549cc623ae02955d921b
AlanNRZBV <alan.noruzbaev@gmail.com>
2026-05-04 22:35:49 +0600

    feat: add error simulation logic and integrate new components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import { Component } from 'react';
 import Layout from './components/Layout/Layout.tsx';
+import Header from './components/Header/Header.tsx';
+import CardList from './components/CardList/CardList.tsx';
+import CustomButton from './components/UI/CustomButton/CustomButton.tsx';
+import type { AppState } from './App.types.ts';
 
-class App extends Component {
+class App extends Component<object, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.state = { throwError: false };
+    this.handleSimulateError = this.handleSimulateError.bind(this);
+  }
+
+  handleSimulateError() {
+    console.error('Simulated error triggered by user');
+    this.setState({ throwError: true });
+  }
+
   render() {
+    if (this.state.throwError) {
+      throw new Error('Simulated application error');
+    }
     return (
       <Layout>
-        <div>test</div>
+        <Header />
+        <CardList />
+        <CustomButton onClick={this.handleSimulateError}>
+          Simulate Error
+        </CustomButton>
       </Layout>
     );
   }
