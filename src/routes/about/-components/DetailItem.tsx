ccd68eefeebd8a2bd88a5f7540031c763cf2e83d
AlanNRZBV <alan.noruzbaev@gmail.com>
2026-06-01 22:40:04 +0600

    feat: add dark mode support to ProjectDetails and DetailItem components

--- src/routes/about/-components/DetailItem.tsx
+++ src/routes/about/-components/DetailItem.tsx
@@ -8,8 +8,10 @@ interface DetailItemProps {
 export const DetailItem: FC<DetailItemProps> = ({ label, children }) => {
   return (
     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">{label}</dt>
-      <dd className="text-gray-700 sm:col-span-2">{children}</dd>
+      <dt className="font-medium text-gray-900 dark:text-gray-300">{label}</dt>
+      <dd className="text-gray-700 dark:text-gray-400 sm:col-span-2">
+        {children}
+      </dd>
     </div>
   );
 };
