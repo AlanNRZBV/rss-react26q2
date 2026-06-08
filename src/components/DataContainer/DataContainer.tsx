60460e2317002a9e467510f784f68e1fe6c0c9b4
AlanNRZBV <alan.noruzbaev@gmail.com>
2026-06-09 01:09:25 +0600

    feat: display message for empty submissions in DataContainer

--- src/components/DataContainer/DataContainer.tsx
+++ src/components/DataContainer/DataContainer.tsx
@@ -5,6 +5,14 @@ const DataContainer = () => {
   const { getSubmissions } = useFormActions();
   const data = getSubmissions();
 
+  if (data.length === 0) {
+    return (
+      <div className="grid grid-cols-4 my-6 gap-2">
+        <h3>nothing to show</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-4 my-6 gap-2">
       {data.map(({ id, submission, createdAt }) => (
