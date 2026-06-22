a0fa244be8bf5f53439340975bc43ed6f4df844d
AlanNRZBV <alan.noruzbaev@gmail.com>
2026-06-22 19:07:49 +0600

    chore: remove all test files and related dependencies cause them are not necessary for this task

--- src/test-utils/test-render.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
-import { ThemeProvider } from '../context/ThemeContext';
-
-export const renderWithProviders = (ui: React.ReactElement) => {
-  return rtlRender(<ThemeProvider>{ui}</ThemeProvider>);
-};
